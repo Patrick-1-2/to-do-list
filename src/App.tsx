450a2af2d90bd6a41691f8e4aa7b2a95cccf1f62
John Patrick J. Del Prado <patrick81304@gmail.com>
2025-08-20 18:33:24 +0800

    finished design

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-blue-950 p-4 min-h-screen flex justify-center items-center'>
+      <div className='max-w-[500px] w-[80%] bg-slate-400 p-4 rounded-md shadow-md'>
+        <h1 className='text-center text-white text-4xl'>To-Do List</h1>
+        <div className='flex gap-2 justify-center my-10'>
+          <input
+          type='text' 
+          placeholder='Add your tasks...'
+          className='flex-[3] border-2 outline-none border-gray-900 text-white placeholder-gray-100 p-2 rounded-md focus:border-white'/>
+          <button className='flex-[1] bg-green-400 border-1 outline-1 border-gray-900 placeholder-gray-800 rounded-md hover:bg-green-500'>
+            Add Task
+          </button>
+        </div>
+        <div>
+        <h1 className='text-xl text-center text-white'>Pending Tasks</h1>
+          <div className='bg-blue-800 p-2 rounded-md flex justify-between items-center my-4 text-white'>
+            <p>Finish to do list</p>
+            <div className='flex items-center gap-2 cursor-pointer'>
+              <button className='outline-2 px-2 bg-green-500 hover:bg-green-600'>
+                √
+              </button>
+              <button className='outline-2 px-2 bg-red-500 hover:bg-red-600'>
+                X
+              </button>
+
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
